b1debd3f40c16cf3bda8301c5eca6644a64ea130
Evgeny Shurupov <bolteg86@ya.ru>
2017-06-15 23:52:21 +0400

    Прикрутил twig

--- project/web/index.php
+++ project/web/index.php
@@ -6,11 +6,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-// ... definitions
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views/',
+    'twig.options' => array(
+        'cache' => __DIR__.'/../views_c/'
+    )
+));
 
-$app->get('/', function () {
+$app->get('/', function () use ($app) {
 
-    return new Response('eee');
+    return $app['twig']->render('index.html.twig', ['var' => 'data']);
 
 });
 
