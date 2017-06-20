b6185af0ee7f163cbacb175cf0c09980876fb4f9
Evgeny Shurupov <bolteg86@ya.ru>
2017-06-20 17:14:51 +0400

    Добавил обработку ошибок

--- project/includes/routes.php
+++ project/includes/routes.php
@@ -7,6 +7,7 @@
  */
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 $app->get('/', function () use ($app) {
 
@@ -16,7 +17,12 @@ $app->get('/', function () use ($app) {
 
 $app->get('/{slug}', function ($slug) use ($app) {
 
-    return $app['twig']->render('page/' . $slug . '.html.twig', ['slug' => $slug]);
+    try {
+        return $app['twig']->render('page/' . $slug . '.html.twig', ['slug' => $slug]);
+    } catch (Twig_Error_Loader $e) {
+        $app->abort(404);
+        return null;
+    }
 
 });
 
@@ -26,4 +32,16 @@ $app->post('/post', function (Request $request) use ($app) {
 
     return $app['twig']->render('page/index.html.twig', ['slug' => '']);
 
+});
+
+$app->error(function (\Exception $e, $headers, $code) use ($app) {
+    switch ($code) {
+        case 404:
+            $message = 'Запрошенная страница не найдена.';
+            break;
+        default:
+            $message = 'Просим прощения, что-то пошло не так.';
+    }
+
+    return $app['twig']->render('page/error.html.twig', ['code' => $code, 'message' => $message]);
 });
