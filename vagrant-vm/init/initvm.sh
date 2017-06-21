783ed0c14531fcdeb7bfadeac4f70b3b2c3858b6
Evgeny Shurupov <bolteg86@ya.ru>
2017-06-21 17:22:36 +0400

    Добавил параметр в одну из команд скрипта установки, который удаляет запрос к пользователю

--- vagrant-vm/init/initvm.sh
+++ vagrant-vm/init/initvm.sh
@@ -100,7 +100,7 @@ if $INSTALL_APACHE; then
 fi
 
 if $INSTALL_PHP; then
-    sudo apt install php-cli php-common php-json php-mysql php-mbstring php-gd php-curl php-zip libapache2-mod-php php-xml php-intl php-mongodb wget curl -y
+    sudo apt install php-cli php-common php-json php-mysql php-mbstring php-gd php-curl php-zip libapache2-mod-php php-xml php-intl php-mongodb wget curl -y -f
 fi
 
 if $INSTALL_PHP && $INSTALL_APACHE; then
