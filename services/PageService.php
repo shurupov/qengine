e39ec5c26461d97e463a371dae5126eb04f77ed6
Evgeny Shurupov <bolteg86@ya.ru>
2018-03-08 13:38:52 +0000

    Admin panels

--- services/PageService.php
+++ services/PageService.php
@@ -11,9 +11,13 @@ namespace Qe;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use ReflectionClass;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -31,12 +35,20 @@ class PageService implements ServiceProviderInterface
     /** @var string $uri **/
     private $uri;
 
+    const STANDARD_ADMIN_PANEL = 'standard';
+    const DATA_ADMIN_PANEL = 'data';
+    const PANELS_ADMIN_PANEL = 'panels';
+
+    const STANDARD_ADMIN_PANEL_PAGE_SLUG = 'page';
+
+    const ADMIN_LAYOUT_TYPE = 'admin';
+
     public function isEditMode()
     {
         return $this->editMode;
     }
 
-    public function renderAdminPanel($adminType, $dataType)
+    public function renderAdminPanel($panelType, $slug, $panelMethod)
     {
 
         if (!$this->editMode) {
@@ -47,14 +59,113 @@ class PageService implements ServiceProviderInterface
             }
         }
 
+        if (!$this->isEditMode()) {
+            return $this->renderBody([], 'admin');
+        }
+
+        switch ($panelType) {
+            case self::STANDARD_ADMIN_PANEL :
+                return $this->renderStandardAdminPanel($slug);
+                break;
+            case self::DATA_ADMIN_PANEL :
+                return $this->renderDataAdminPanel($slug);
+                break;
+            case self::PANELS_ADMIN_PANEL :
+                return $this->renderPanelsAdminPanel($slug, $panelMethod);
+                break;
+        }
+
+        throw new NotFoundHttpException();
+    }
+
+    private function renderPanelsAdminPanel($panel, $method)
+    {
+        $panelFound = false;
+
+        $panels = [];
+        $panelPath = __DIR__.'/../extensions/panels/';
+        if (is_dir($panelPath) && $dh = opendir($panelPath)) {
+            while (($dir = readdir($dh)) !== false) {
+                if ($dir == "." || $dir == "..") {
+                    continue;
+                }
+                $panelFound = $panelFound || ($dir == $panel);
+                $settings = json_decode( file_get_contents($panelPath.$dir.'/settings.json'), true );
+                $panels[] = [
+                    'name' => $dir,
+                    'title' => $settings['title']
+                ];
+            }
+        }
+
+        if (count($panels) == 0 || (!empty($panel) && !$panelFound)) {
+            throw new NotFoundHttpException();
+        }
+
+        if (empty($panel)) {
+            return new RedirectResponse($this->app['settings']['admin']['page']['uri'].'/'.self::PANELS_ADMIN_PANEL.'/'.$panels[0]['name']);
+        }
+
+        $response = $this->executePanelMethod($panel, $method);
+
+        if ($response instanceof Response) {
+            return $response;
+        }
+
         return $this->renderBody([
-            'adminType' => $adminType,
-            'dataType' => $dataType,
-            'additionalCollection' => ( array_key_exists($dataType, $this->app['settings']['additionalData']) ? $this->app['dataService']->getDocuments($dataType, [], ['_id' => -1]) : [] ),
-            'pageList' => ( $dataType == 'page' ? $this->app['dataService']->getDocuments('page') : [] ),
-            'menu' => ( ($dataType == 'menu' || $dataType == 'page') ? $this->app['dataService']->getDocuments('menu') : []),
-            'formfields' => ( $dataType == 'form' ? $this->app['dataService']->getDocuments('formfields') : [] )
+            'adminType' => self::PANELS_ADMIN_PANEL,
+            'panel' => $panel,
+            'method' => $method,
+            'panels' => $panels,
+            'panelData' => $response
         ], 'admin');
+    }
+
+    private function renderStandardAdminPanel($slug)
+    {
+        if (empty($slug)) {
+            return new RedirectResponse($this->app['settings']['admin']['page']['uri'].'/'.self::STANDARD_ADMIN_PANEL.'/'.self::STANDARD_ADMIN_PANEL_PAGE_SLUG);
+        }
+
+        $data = [
+            'adminType' => self::STANDARD_ADMIN_PANEL,
+            'dataType' => $slug,
+            'list' => $this->app['dataService']->getDocuments($slug)
+        ];
+
+        if ($slug = self::STANDARD_ADMIN_PANEL_PAGE_SLUG) {
+            $data['menu'] = $this->app['dataService']->getDocuments('menu');
+        }
+
+        return $this->renderBody($data, self::ADMIN_LAYOUT_TYPE);
+    }
+
+    private function renderDataAdminPanel($dataType)
+    {
+        if (empty($dataType)) {
+            foreach ($this->app['settings']['additionalData'] as $collectionName=>$collectionTitle) {
+                return new RedirectResponse($this->app['settings']['admin']['page']['uri'].'/'.self::DATA_ADMIN_PANEL.'/'.$collectionName);
+            }
+        }
+
+        $renderData = [
+            'adminType' => self::DATA_ADMIN_PANEL,
+            'dataType' => $dataType
+        ];
+
+        if (array_key_exists($dataType, $this->app['settings']['additionalData'])) {
+            $renderData['additionalCollection'] = $this->app['dataService']->getDocuments($dataType, [], ['_id' => -1]);
+        }
+
+        $pages = $this->app['dataService']->getDocuments('page');
+        foreach ($pages as $page) {
+            if ($page->display == $dataType) {
+                $renderData['linkPrefix'] = $page->uri . '/';
+                break;
+            }
+        }
+
+        return $this->renderBody($renderData, self::ADMIN_LAYOUT_TYPE);
     }
 
     public function logout()
@@ -213,5 +324,81 @@ class PageService implements ServiceProviderInterface
             $settings[$setting->key] = $setting;
         }
         return $settings;
+    }
+
+    private function executePanelMethod($panel, $method)
+    {
+        $panelPath = __DIR__.'/../extensions/panels/'.$panel.'/';
+        $className = strtoupper($panel[0]).substr($panel, 1).'PanelController';
+
+        if (file_exists($panelPath.$className.'.php')) {
+
+            require_once $panelPath . $className . '.php';
+
+            if (class_exists($className)) {
+
+                $reflectionClass = new ReflectionClass($className);
+                $reflectionMethods = $reflectionClass->getMethods();
+
+                foreach ($reflectionMethods as $reflectionMethod) {
+                    if ($method == preg_replace('/^([\w\d]+)(Redirect|Json)$/', '${1}', $reflectionMethod->name)) {
+
+                        $reflectionParameters = $reflectionMethod->getParameters();
+
+                        $parameters = [];
+
+                        foreach ($reflectionParameters as $reflectParameter) {
+                            switch ($reflectParameter->name) {
+                                case 'get':
+                                case 'GET':
+                                    $parameters [] = $this->request->query->all();
+                                    break;
+                                case 'post':
+                                case 'POST':
+                                    $parameters [] = $this->request->request->all();
+                                    break;
+                                case 'files':
+                                case 'FILES':
+                                    $parameters [] = $this->request->files->all();
+                                    break;
+                                case 'dataService':
+                                case 'dataservice':
+                                    $parameters [] = $this->app['dataService'];
+                                    break;
+                                case 'pageService':
+                                case 'pageservice':
+                                    $parameters [] = $this;
+                                    break;
+                                case 'postService':
+                                case 'postservice':
+                                    $parameters [] = $this->app['postService'];
+                                    break;
+                            }
+                        }
+
+                        $response = $reflectionMethod->invokeArgs(new $className($this->app), $parameters);
+
+                        if (preg_match('/^([\w\d]+?)(Redirect|Json)?$/', $reflectionMethod->name, $matches)) {
+
+                            $type = $matches[2];
+
+                            switch ($type) {
+                                case 'Redirect' :
+                                    return new RedirectResponse($response);
+                                    break;
+                                case 'Json' :
+                                    return new JsonResponse($response);
+                                    break;
+                            }
+
+                        }
+
+                        return $response;
+                    }
+                }
+            }
+        }
+
+        throw new BadRequestHttpException();
     }
 }
