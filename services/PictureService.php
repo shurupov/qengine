814903ae6b05657a46c621fa92828377b217ebc2
Evgeny Shurupov <bolteg86@ya.ru>
2018-03-17 16:06:35 +0400

    Починил недобавление изменённых картинок из корня

--- services/PictureService.php
+++ services/PictureService.php
@@ -38,7 +38,7 @@ class PictureService implements ServiceProviderInterface
 
             $pathInfo = pathinfo($sourceUri);
 
-            $folder = str_replace('/source/', '/source/generated/', $pathInfo['dirname']);
+            $folder = str_replace('/source', '/source/generated', $pathInfo['dirname']);
             $filename = $pathInfo['filename'].'-'.$targetSettings['width'].'x'.$targetSettings['height'].'.'.$pathInfo['extension'];
 
             $this->resizeAndCrop(
@@ -55,7 +55,7 @@ class PictureService implements ServiceProviderInterface
         elseif (!empty($targetSettings['width']) && empty($targetSettings['height'])) { //Width exists, height doesn't
             $pathInfo = pathinfo($sourceUri);
 
-            $folder = str_replace('/source/', '/source/generated/', $pathInfo['dirname']);
+            $folder = str_replace('/source', '/source/generated', $pathInfo['dirname']);
             $filename = $pathInfo['filename'].'-w'.$targetSettings['width'].'.'.$pathInfo['extension'];
 
             $this->resize(
@@ -70,7 +70,7 @@ class PictureService implements ServiceProviderInterface
         elseif (empty($targetSettings['width']) && !empty($targetSettings['height'])) { //Height exists, width doesn't
             $pathInfo = pathinfo($sourceUri);
 
-            $folder = str_replace('/source/', '/source/generated/', $pathInfo['dirname']);
+            $folder = str_replace('/source', '/source/generated', $pathInfo['dirname']);
             $filename = $pathInfo['filename'].'-h'.$targetSettings['height'].'.'.$pathInfo['extension'];
 
             $this->resize(
@@ -90,7 +90,7 @@ class PictureService implements ServiceProviderInterface
 
     private function createThumb($sourcePath)
     {
-        $thumbPath = str_replace('/source/', '/thumbs/', $sourcePath);
+        $thumbPath = str_replace('/source', '/thumbs', $sourcePath);
 
         $pathInfo = pathinfo($thumbPath);
 
