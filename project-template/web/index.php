f40d3750c46181f66ffd85902e82d02d703636fe
Evgeny Shurupov <bolteg86@ya.ru>
2019-01-22 00:26:48 +0400

    project template changes

--- project-template/web/index.php
+++ project-template/web/index.php
@@ -1,29 +1,10 @@
 <?php
 
-//use Symfony\Component\Yaml\Yaml;
-
 define('QENGINE_INDEX_PATH', __DIR__);
-define('QENGINE_ROOT_PATH', __DIR__ . '/qengine');
+define('QENGINE_ROOT_PATH', __DIR__ . '/..');
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once QENGINE_ROOT_PATH . '/vendor/autoload.php';
 
-$app = new \QEngine\Core\App();
-
-//$app = new Silex\Application();
-
-//$app['settings'] = Yaml::parseFile('../settings/settings.yml');
-
-//setlocale(LC_ALL, $app['settings']['locale']);
-
-//require_once __DIR__.'/../includes/register.php';
-
-require_once __DIR__ . '/../includes/twigExtensions.php';
-require_once __DIR__ . '/../includes/controllerExtensions.php';
-
-//$app['debug'] = $app['settings']['debug'];
-
-//require_once __DIR__.'/../includes/routes.php';
+$app = new QEngine\Core\App();
 
 $app->run();
-
-//$app['logger']->addRecord(200, $logger->dump());
